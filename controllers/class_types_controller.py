db523dc5498687386e14ef1983a386d2d9124648
AnnaW86 <annawebbstewart@gmail.com>
2022-04-27 21:12:52 +0100

    added 'create' route for class_types with relevant html

--- controllers/class_types_controller.py
+++ controllers/class_types_controller.py
@@ -14,3 +14,13 @@ def class_types():
 @class_types_blueprint.route("/class_types/new")
 def new_class_type():
     return render_template("class_types/new.html")
+
+# CREATE
+@class_types_blueprint.route("/class_types", methods=['POST'])
+def create_class_type():
+    title = request.form['title']
+    intensity = request.form['intensity']
+    description = request.form['description']
+    class_type = ClassType(title, intensity, description)
+    class_type_repository.save(class_type)
+    return redirect('/class_types')
